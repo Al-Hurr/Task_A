47f85b7fd1344d58546e622b397f62b7c8aaf4c0
Azat Azat <freedoom9654@gmail.com>
2023-04-06 22:25:22 +0300

    added Task_A_1 project

---
+++ Task_A_1/NodeTree.cs
@@ -0,0 +1,85 @@
+﻿
+using System.Collections.Generic;
+
+namespace Task_A_1
+{
+    public class NodeTree
+    {
+        public int Data { get; set; }
+        public NodeTree Left { get; set; }
+        public NodeTree Rigth { get; set; }
+
+        public static void PrintTreeToDepth(NodeTree nodeTree)
+        {
+            if(nodeTree == null)
+            {
+                return;
+            }
+
+            System.Console.Write($"{nodeTree.Data} ");
+
+            PrintTreeToDepth(nodeTree.Left);
+            PrintTreeToDepth(nodeTree.Rigth);
+        }
+
+        public static void PrintTreeToWidth(NodeTree nodeTree)
+        {
+            if(nodeTree == null)
+            {
+                return;
+            }
+
+            Queue<NodeTree> nodesQueue = new Queue<NodeTree>();
+            nodesQueue.Enqueue(nodeTree);
+
+            while(nodesQueue.Count > 0)
+            {
+                NodeTree node = nodesQueue.Dequeue();
+
+                System.Console.Write($"{node.Data} ");
+
+                if(node.Left != null)
+                {
+                    nodesQueue.Enqueue(node.Left);
+                }
+                if(node.Rigth != null)
+                {
+                    nodesQueue.Enqueue(node.Rigth);
+                }
+            }
+        }
+
+        public void PutNode(NodeTree node)
+        {
+            if (node == null
+                || node.Data == Data)
+            {
+                return;
+            }
+
+            if(node.Data < Data)
+            {
+                if(Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.PutNode(node);
+                }
+            }
+
+            if(node.Data > Data)
+            {
+                if(Rigth == null)
+                {
+                    Rigth = node;
+                }
+                else
+                {
+                    Rigth.PutNode(node);
+                }
+            }
+        }
+    }
+}
---
+++ Task_A_1/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+
+namespace Task_A_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NodeTree node = new NodeTree
+            {
+                Data = 8,
+                Left = new NodeTree
+                {
+                    Data = 3,
+                    Left = new NodeTree
+                    {
+                        Data = 1
+                    },
+                    Rigth = new NodeTree
+                    {
+                        Data = 6,
+                        Left = new NodeTree
+                        {
+                            Data = 4
+                        },
+                        Rigth = new NodeTree
+                        {
+                            Data = 7
+                        }
+                    }
+                },
+                Rigth = new NodeTree
+                {
+                    Data = 10,
+                    Rigth = new NodeTree
+                    {
+                        Data = 14,
+                        Left = new NodeTree
+                        {
+                            Data = 13
+                        }
+                    }
+                }
+            };
+
+            NodeTree.PrintTreeToDepth(node);
+            Console.WriteLine();
+            NodeTree.PrintTreeToWidth(node);
+
+            Console.ReadLine();
+        }
+    }
+}
